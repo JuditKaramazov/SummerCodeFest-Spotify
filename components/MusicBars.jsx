4eef69c407c81b355584bc6e3e1b7cabeeade151
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-07-10 01:48:31 +0200

    Improve login visuals

---
+++ components/MusicBars.jsx
@@ -0,0 +1,65 @@
+import styled, { keyframes } from "styled-components";
+import { useEffect, useState, useRef } from "react";
+import useElementSize from "@/hooks/useElementSize";
+
+const Container = styled.ul`
+  height: clamp(30px, 10vh, 60px);
+  width: 100%;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  flex: 0 0;
+`;
+
+const barAnimation = keyframes`
+  0% {height: 5%}
+  20% {height: 70%}
+  40% {height: 100%}
+  60% {height: 30%}
+  80% {height: 50%}
+  100% {height: 10%}
+`;
+
+const Bar = styled.li`
+  height: 100%;
+  width: 20px;
+  list-style: none;
+  margin: 0 4px;
+  padding: 0;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  background-color: #1ed760;
+  animation: ${barAnimation} 2s ease-in-out infinite reverse;
+  animation-delay: ${({ delay }) => delay}s;
+`;
+
+const MusicBars = () => {
+  const container = useRef(null);
+  const bars = useRef([]);
+  const [barsNumber, setBarsNumber] = useState(0);
+  const containerDimensions = useElementSize(container);
+
+  useEffect(() => {
+    const { width } = containerDimensions || { width: 1200 };
+    const barsNum = Math.round(width / 28);
+    setBarsNumber(barsNum);
+  }, [containerDimensions]);
+
+  useEffect(() => {
+    const MAX_BARS = 50;
+    bars.current = Array(MAX_BARS)
+      .fill(null)
+      .map((_, i) => <Bar key={i} delay={-(Math.random() * 2)} />);
+  }, []);
+
+  return (
+    <Container ref={container}>
+      {bars.current.slice(0, barsNumber)}
+    </ Container>
+  );
+};
+
+export default MusicBars;
---
+++ hooks/useElementSize.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function useElementSize(refToObserve) {
+  const [dimensions, setDimensions] = useState(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      const { height, width } = entries[0].contentRect;
+      setDimensions({ height, width });
+    });
+
+    if (refToObserve.current) {
+      observer.observe(refToObserve.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [refToObserve.current]);
+
+  return dimensions;
+}
